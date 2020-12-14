71fda0cbcf1e3abef37302773a2ec7bedbe667a8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-12-14 11:09:29 +0000

    t   Trivial doctest code for https://youtrack.jetbrains.com/issue/CPP-1420

--- examples/doctest_existing_main/DoctestApprovalsTests.cpp
+++ examples/doctest_existing_main/DoctestApprovalsTests.cpp
@@ -15,3 +15,11 @@ TEST_CASE("TestStreamableObject")
 {
     Approvals::verify(42);
 }
+
+TEST_CASE("CLion demo")
+{
+    // Trivial example code for https://youtrack.jetbrains.com/issue/CPP-1420
+    int x = 2;
+    int xsquared = x * x;
+    CHECK(xsquared == 4);
+}
